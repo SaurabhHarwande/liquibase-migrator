a148d71ad7321c51e15844e7e6cf3f0750052a82
Saurabh Harwande <saurabhharwande@gmail.com>
2021-04-02 21:56:31 +0530

    Add more commands Reorder Dockerfile for faster compilation Add docker ignore file Add a base migrations file

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
 FROM alpine:3.13.3
 RUN apk add --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/ liquibase
-COPY ./liquibase-migrator.sh /usr/local/bin/liquibase-migrator
-WORKDIR /liquibase-migrator
-#This should be removed. Only using it for development
-COPY ./postgresql-42.2.19.jar .
+#TODO: Move ENV variable out of docker file after development
 ENV MIGRATIONS_BASE_DIRECTORY=DatabaseMigrations
 ENV MIGRATIONS_STORE_SIRECTORY=Migrations
 ENV MIGRATIONS_FORMAT=yaml
-ENV MIGRATIOS_BASE_FILE=db-migrations.${MIGRATIONS_FORMAT}
-ENV DB_SNAPSHOT_FILE=db-snapshot
+ENV MIGRATIONS_MASTERLOG_FILE=master-change-log.${MIGRATIONS_FORMAT}
+ENV DB_SNAPSHOT_FILE=db-snapshot.${MIGRATIONS_FORMAT}
 ENV DB_TYPE=postgresql
 ENV DB_NAME=postgres3
 ENV DB_URL=jdbc:${DB_TYPE}://local-postgres.dns.podman:5432/${DB_NAME}
 ENV DB_USERNAME=postgres
-ENV DB_PASSWORD=*****
+ENV DB_PASSWORD=RandomTown
 ENV DB_DRIVER=postgresql-42.2.19.jar
+WORKDIR /liquibase-migrator
+COPY ./postgresql-42.2.19.jar .
+COPY ./master-change-log.yaml .
+COPY ./liquibase-migrator.sh /usr/local/bin/liquibase-migrator
+#This should be removed. Only using it for development
 ENTRYPOINT ["liquibase-migrator"]
--- liquibase-migrator.sh
+++ liquibase-migrator.sh
@@ -1,19 +1,82 @@
 #!/bin/sh
-echo ${@}
+#TODO: Implement(google if possible) a simple command to read inline parameter and use them to override ENVIRONMENT variables.
 case ${1} in
     initialize)
-        #Add a check if snapshot already exists before initializing. I we override a snapshot we will lose a change step
-        liquibase --logLevel debug --classpath ${DB_DRIVER} --url ${DB_URL} --username ${DB_USERNAME} --password ${DB_PASSWORD} --outputFile ./${MIGRATIONS_BASE_FOLDER}/${DB_SNAPSHOT_FILE}.${MIGRATIONS_FORMAT} snapshot --snapshotFormat ${MIGRATIONS_FORMAT}
+        #TODO: Check if snapshot already exists
+        #Copy over master change log
+        cp \
+            ./${MIGRATIONS_MASTERLOG_FILE} \
+            ./${MIGRATIONS_BASE_DIRECTORY}/${MIGRATIONS_MASTERLOG_FILE}
+        #Create base snapshot
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password "${DB_PASSWORD}" \
+            --outputFile ./${MIGRATIONS_BASE_DIRECTORY}/${DB_SNAPSHOT_FILE} \
+            snapshot \
+            --snapshotFormat ${MIGRATIONS_FORMAT}
+        #Sync initial change log to database
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password ${DB_PASSWORD} \
+            --changeLogFile ./${MIGRATIONS_BASE_DIRECTORY}/${MIGRATIONS_MASTERLOG_FILE} \
+            changeLogSync
         ;;
     generate)
-        liquibase --referenceUrl ${DB_URL} --referenceUsername ${DB_USERNAME} --referencePassword ${DB_PASSWORD} --url \"offline:${DB_TYPE}?snapshot=./${MIGRATIONS_BASE_FOLDER}/${DB_SNAPSHOT_FILE}.${MIGRATIONS_FORMAT}\" --changeLogFile ./${MIGRATIONS_BASE_FOLDER}/${MIGRATIONS_STORE_FOLDER}/${2}.${MIGRATIONS_FORMAT} diffChangeLog
-        liquibase --outputFile ${DB_SNAPSHOT_FILE}.${MIGRATIONS_FORMAT} snapshot --snapshotFormat ${MIGRATIONS_FORMAT}
+        #TODO: Check if changelog already exists
+        #Generate new change log file
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --referenceUrl ${DB_URL} \
+            --referenceUsername ${DB_USERNAME} \
+            --referencePassword ${DB_PASSWORD} \
+            --url \"offline:${DB_TYPE}?snapshot=./${MIGRATIONS_BASE_DIRECTORY}/${DB_SNAPSHOT_FILE}\" \
+            --changeLogFile ./${MIGRATIONS_BASE_DIRECTORY}/${MIGRATIONS_STORE_FOLDER}/${2}.${MIGRATIONS_FORMAT} \
+            diffChangeLog
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password ${DB_PASSWORD} \
+            --outputFile ./${MIGRATIONS_BASE_DIRECTORY}/${DB_SNAPSHOT_FILE} \
+            snapshot \
+            --snapshotFormat ${MIGRATIONS_FORMAT}
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password ${DB_PASSWORD} \
+            --changeLogFile ./${MIGRATIONS_BASE_DIRECTORY}/${MIGRATIONS_MASTERLOG_FILE} \
+            changeLogSync
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password ${DB_PASSWORD} \
+            tag ${2}
         ;;
     update)
-        liquibase --logLevel debug --classpath ${DB_DRIVER} --url ${DB_URL} --username ${DB_USERNAME} --password ${DB_PASSWORD} --changeLogFile ./${MIGRATIONS_BASE_FOLDER}/db-change-log.${MIGRATIONS_FORMAT} update
+        #TODO: Add a if else for running update/updateToTag commands
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password ${DB_PASSWORD} \
+            --changeLogFile ./${MIGRATIONS_BASE_DIRECTORY}/${MIGRATIONS_MASTERLOG_FILE} \
+            update
         ;;
     rollback)
-        echo "code to rollback to a certain state"
+        #Rollback the database to a mentioned tag
+        liquibase \
+            --classpath ${DB_DRIVER} \
+            --url ${DB_URL} \
+            --username ${DB_USERNAME} \
+            --password ${DB_PASSWORD} \
+            --changeLogFile ./${MIGRATIONS_BASE_DIRECTORY}/${MIGRATIONS_MASTERLOG_FILE} \
+            rollback ${2}
         ;;
     *)
         liquibase ${@}
